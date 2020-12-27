fed3d07c536e3155e6267be3b5123420ac7db658
Dimitar Andreev <D.Andreev90@gmail.com>
2020-12-27 10:57:06 +0200

    Update todo endpoint (#4)

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
   "encoding/json"
   "fmt"
+  "github.com/pkg/errors"
+  "io"
   "io/ioutil"
   "net/http"
   "os"
+  "strconv"
+  "strings"
 )
 
 var todos = []Todo{
@@ -21,6 +25,7 @@ var statusNames = map[TodoStatus]string{
 
 type TodoBody struct {
   Name string `json:"name"`
+  Status TodoStatus `json:"status"`
 }
 
 func checkErr(e error) {
@@ -30,7 +35,7 @@ func checkErr(e error) {
 }
 
 func main() {
-  http.HandleFunc("/todo", todoController)
+  http.HandleFunc("/todo/", todoController)
   port := getPort()
 
   fmt.Println("Server listening at: ", port)
@@ -58,22 +63,97 @@ func deserializeTodoBody(b []byte, parseBody *TodoBody) error {
 
 func todoController(w http.ResponseWriter, req *http.Request) {
   if req.Method == "POST" {
-    b, err := ioutil.ReadAll(req.Body)
-    checkErr(err)
-
-    var parseBody TodoBody
-    e := deserializeTodoBody(b, &parseBody)
-    if e != nil {
-      http.Error(w, "Invalid todo data", http.StatusInternalServerError)
+    err := addTodo(req.Body)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
       return
     }
-    todos = append(todos, newTodo(len(todos) + 1, parseBody.Name))
+
     w.WriteHeader(http.StatusOK)
   } else if req.Method == "GET" {
-    b, err := json.Marshal(&todos)
-    checkErr(err)
-
+    todos := serializeTodos()
     w.Header().Set("Content-Type", "application/json")
-    w.Write(b)
+    w.Write(todos)
+  } else if req.Method == "PUT" {
+    todoBS := updateTodo(w, req)
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(todoBS)
+    w.WriteHeader(http.StatusOK)
   }
+}
+
+func updateTodo(w http.ResponseWriter, req *http.Request) []byte {
+  splitPath := strings.Split(req.URL.Path, "/")
+  id, err := strconv.Atoi(splitPath[len(splitPath) - 1])
+  if err != nil {
+    http.Error(w, "Invalid todo id", http.StatusBadRequest)
+    return nil
+  }
+
+  idx := findTodoIdx(id)
+  if idx == -1 {
+    http.Error(w, "Todo does not exists", http.StatusBadRequest)
+    return nil
+  }
+
+  b, err := ioutil.ReadAll(req.Body)
+  if err != nil {
+    http.Error(w, "Invalid body", http.StatusBadRequest)
+    return nil
+  }
+  var parsedBody TodoBody
+  e := deserializeTodoBody(b, &parsedBody)
+  if e != nil {
+    http.Error(w, "Invalid todo update data", http.StatusBadRequest)
+    return nil
+  }
+  todos[idx].Name = parsedBody.Name
+  todos[idx].Status = parsedBody.Status
+  todoBS, err := serializeTodo(todos[idx])
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return nil
+  }
+
+  return todoBS
+}
+
+func findTodoIdx(id int) int {
+  idx := -1
+  for i, todo := range todos {
+    if todo.Id == id {
+      idx = i
+    }
+  }
+
+  return idx
+}
+
+func addTodo(body io.ReadCloser) error {
+  b, err := ioutil.ReadAll(body)
+  checkErr(err)
+
+  var parseBody TodoBody
+  e := deserializeTodoBody(b, &parseBody)
+  if e != nil {
+    return errors.New("Invalid todo data")
+  }
+  todos = append(todos, newTodo(len(todos) + 1, parseBody.Name))
+  return nil
+}
+
+func serializeTodos() []byte {
+  b, err := json.Marshal(&todos)
+  checkErr(err)
+
+  return b
+}
+
+func serializeTodo(todo Todo) ([]byte, error) {
+  b, err := json.Marshal(&todo)
+  if err != nil {
+    return []byte{}, err
+}
+
+  return b, nil
 }
